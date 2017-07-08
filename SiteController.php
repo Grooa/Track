f397b7caf89283471e496ff77c7a2fa6c7bc5e59
Fredrik F Lindhagen <fred.lindh96@gmail.com>
2017-07-08 15:24:25 +0200

    Move Payment logic

--- SiteController.php
+++ SiteController.php
@@ -3,17 +3,11 @@
 namespace Plugin\Track;
 
 use Ip\Exception;
-use PayPal\Api\Amount;
-use PayPal\Api\Item;
-use PayPal\Api\ItemList;
-use PayPal\Api\Transaction;
+use Plugin\GrooaPayment\Models\PayPalModel;
 use Plugin\Track\Models\AwsS3Model;
 use Plugin\Track\Models\TrackModel;
 
-use Plugin\GrooaPayment\Models\PayPalModel;
 use Plugin\GrooaPayment\Models\TrackOrderModel;
-use Plugin\GrooaPayment\Response\BadRequest;
-use Plugin\GrooaPayment\Response\RestError;
 
 class SiteController
 {
@@ -57,7 +51,11 @@ class SiteController
                 'purchasedOn' => !empty($order) && $hasPurchased ?
                     $order['paymentExecuted'] :
                     null,
-                'hasPurchased' => $hasPurchased
+                'hasPurchased' => $hasPurchased,
+                'payPalCheckout' =>
+                    ipUser()->isLoggedIn() && !$hasPurchased ?
+                        PayPalModel::getCheckoutView($trackId) :
+                        null
             ])->render());
         $layout->setLayout('track.php');
 
@@ -112,185 +110,5 @@ class SiteController
         $layout->setLayoutVariable('coverTitle', $track['title']);
 
         return $layout;
-    }
-
-    // TODO:ffl - Move to it's own plugin (GrooaPayment)
-    public function createPayment()
-    {
-        ipRequest()->mustBePost();
-
-        if (!ipUser()->isLoggedIn()) {
-            return self::respondForbidden("User not logged in");
-        }
-
-        $trackId = ipRequest()->getQuery('track'); // Get referenced track
-
-        if (empty($trackId) || !is_numeric($trackId)) {
-            return new BadRequest(['error' => "Missing query `track` or value is non-numeric"]);
-        }
-
-        // Prevent a user from double purchasing a track
-        if (TrackOrderModel::hasPurchased($trackId, ipUser()->userId())) {
-            return new BadRequest(['error' => "You already own this track ($trackId)"]);
-        }
-
-        $transaction = self::generateTransactionForTrack($trackId);
-
-        try {
-            $payment = PayPalModel::createPayment($transaction);
-        } catch (\PayPal\Exception\PayPalConnectionException $pce) {
-            return new RestError($pce->getData(), $pce->getCode());
-        }
-
-        $id = $payment->getId();
-
-        TrackOrderModel::create([
-            'userId' => ipUser()->userId(),
-            'trackId' => $trackId,
-            'paymentId' => $id
-        ]);
-
-        return new \Ip\Response\Json(['paymentID' => $id]);
-    }
-
-    public function executePayment()
-    {
-        ipRequest()->mustBePost();
-
-        if (!ipUser()->isLoggedIn()) {
-            return self::respondForbidden("User must be logged in");
-        }
-
-        $paymentId = ipRequest()->getPost('paymentID');
-        $payerId = ipRequest()->getPost('payerID');
-
-        if (empty($paymentId) || empty($payerId)) {
-            return new BadRequest(['error' => 'Missing paymentID or payerID']);
-        }
-
-        $trackId = ipRequest()->getQuery('track'); // Get referenced track
-
-        if (empty($trackId) || !is_numeric($trackId)) {
-            return new BadRequest(['error' => "Missing query `track` or value is non-numeric"]);
-        }
-
-        $uid = ipUser()->userId();
-        // Get the order created in createPayment
-        $order = TrackOrderModel::getByTrackAndUser($trackId, $uid);
-
-        if (empty($order)) {
-            return new BadRequest(['error' => "User has not yet created an order for track: $trackId"]);
-        }
-
-        // Expect paymentId created in createPayment to match
-        // paymentId gotten from PayPal
-        if ($order['paymentId'] != $paymentId) {
-            $oldId = $order['paymentId'];
-            return new BadRequest([
-                'error' => "Stored paymentId: $oldId, does not match paypal's paymentId: $paymentId"
-            ]);
-        }
-
-        $transaction = self::generateTransactionForTrack($trackId);
-
-        // Do not execute a payment, without a transaction
-        if (empty($transaction)) {
-            return new BadRequest(['error' => "Cannot find track with id: $trackId"]);
-        }
-
-        try {
-            $payment = PayPalModel::executePayment($paymentId, $payerId, $transaction);
-        } catch (Exception $e) {
-            return new RestError(['error' => $e->getMessage()]);
-        }
-
-        $sale = $payment->getTransactions()[0]->getRelatedResources()[0]->getSale();
-
-        $saleId = $sale->getId();
-        $saleState = $sale->getState();
-
-        // TODO:ffl - Continue here, streamline logic
-        // TODO:ffl - Has completed the logic such that a executed payment
-        // TODO:ffl - is stored as a pending order in the databse
-
-        if ($saleState == 'pending' || $saleState == 'completed') {
-            // Update the table with the extra values to track
-            TrackOrderModel::update($order['orderId'], [
-                'payerId' => $payerId,
-                'saleId' => $saleId,
-                'state' => 'pending',
-                'paymentExecuted' => date('Y-m-d H:i:s')
-            ]);
-
-            if ($saleState == 'completed') {
-                // Completes the order, and stores the timestamp
-                TrackOrderModel::completeOrder($order['orderId']);
-            }
-        } else {
-            // If it's not completed or pending it could be an error
-            TrackOrderModel::update($order['orderId'], ['state' => 'cancelled']);
-            return new BadRequest(['error' => "Invalid purchase state: $saleState"]);
-        }
-
-        return new \Ip\Response\Json(['saleId' => $saleId, 'state' => $saleState]);
-    }
-
-    /**
-     * Will find the correct track, and convert it
-     * to a PayPal-friendly Transaction object
-     *
-     * @param int $trackId
-     * @return null|\PayPal\Api\Transaction
-     */
-    private function generateTransactionForTrack($trackId)
-    {
-        $track = TrackModel::get($trackId);
-
-        if (empty($track)) {
-            return null;
-        }
-
-        $amount = new Amount();
-        $amount->setCurrency('EUR')// We work with Euro
-        ->setTotal($track['price']);
-
-        $item = new Item();
-        $item->setCurrency('EUR')
-            ->setQuantity(1)
-            ->setName($track['title'])
-            ->setDescription($track['shortDescription'])
-            ->setPrice($track['price']);
-
-        $list = new ItemList();
-        $list->addItem($item);
-
-        $transaction = new Transaction();
-        $transaction
-            ->setDescription("Online courses at grooa.com")
-            ->setItemList($list)
-            ->setAmount($amount);
-
-        return $transaction;
-    }
-
-
-    private static function respondForbidden($msg, $data = [])
-    {
-        $data['error'] = $msg;
-        $forbidden = new \Ip\Response\Json($data);
-        $forbidden->setStatusCode(403);
-
-        return $forbidden;
-    }
-
-    public function successPayment()
-    {
-
-        die(json_encode(ipRequest()->getPost()));
-    }
-
-    public function cancelPayment()
-    {
-        die(json_encode(ipRequest()->getPost()));
     }
 }
--- view/retrieve.php
+++ view/retrieve.php
@@ -3,6 +3,8 @@
     <a href="<?=ipConfig()->baseUrl()?>tracks/<?=$track['trackId']?>" class="currentPage"><?=$track['title']?></a>
 </nav>
 
+<?=ipSlot('socialshare')?>
+
 <h1><?= $track['title'] ?></h1>
 <div class="introduction"><?= $track['longDescription'] ?></div>
 
@@ -50,52 +52,4 @@
     </ul>
 </section>
 
-<?php if (ipUser()->isLoggedIn() && !$hasPurchased): ?>
-    <?php // No reason to load paypal before user has logged in ?>
-    <script src="https://www.paypalobjects.com/api/checkout.js"></script>
-
-    <script type="text/javascript">
-        var CREATE_PAYMENT_URL = '<?=ipConfig()->baseUrl()?>paypal/create-payment?track=<?=$track['trackId']?>';
-        var EXECUTE_PAYMENT_URL = '<?=ipConfig()->baseUrl()?>paypal/execute-payment?track=<?=$track['trackId']?>';
-
-        paypal.Button.render({
-
-            env: 'sandbox', // Or 'production'
-
-            commit: true, // Show a 'Pay Now' button
-
-            style: {
-                size: 'responsive',
-                color: 'blue',
-                shape: 'pill',
-                label: 'checkout'
-            },
-
-            payment: function () {
-                return paypal.request.post(CREATE_PAYMENT_URL).then(function (data) {
-                    return data.paymentID; // Returned from local REST-API
-                });
-            },
-
-            onAuthorize: function (data) {
-                return paypal.request.post(EXECUTE_PAYMENT_URL, {
-                    paymentID: data.paymentID,
-                    payerID: data.payerID
-                }).then(function () {
-                    location.reload();
-
-                    // The payment is complete!
-                    // You can now show a confirmation message to the customer
-                });
-            },
-
-            onCancel: function (data, actions) {
-                console.log('cancelled');
-                console.log(data);
-                console.log(actions);
-            }
-
-        }, '#paypal-button');
-    </script>
-
-<?php endif; ?>
+<?=!empty($payPalCheckout) ? $payPalCheckout : ''?>
